7dc54353f21d578fe7c609f0bfe97c8effca0f75
Назар Остафійчук <ostafii4uk@gmail.com>
2024-02-22 11:11:01 +0200

    Added loading for meals page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,19 +1,34 @@
+import { Suspense } from 'react';
+import { getMeals } from '@/lib/meals'
 import Link from 'next/link';
-import styles from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid';
+import MealsLoadingPage from './loading-out';
+import styles from './page.module.css'
+
+
+async function Meals() {
+  const meals = await getMeals()
+
+  return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
-        <h1>Delicious meals, created <span className={styles.highlight}>by you</span></h1>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={styles.highlight}>by you</span>
+        </h1>
         <p>Choose your favorite recipe and cook</p>
         <p className={styles.cta}>
           <Link href='/meals/share'>Share your favorite recipe</Link>
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   )
