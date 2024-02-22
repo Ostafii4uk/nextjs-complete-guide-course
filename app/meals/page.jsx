362fea8636ad80b0d240ded906671bbf5c8f6e5e
Назар Остафійчук <ostafii4uk@gmail.com>
2024-02-22 10:33:08 +0200

    Updated meals page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,11 +1,20 @@
 import Link from 'next/link';
+import styles from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid';
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals page</h1>
-      <Link href='./'>Back</Link>
-      <Link href='/meals/share'>Share</Link>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>Delicious meals, created <span className={styles.highlight}>by you</span></h1>
+        <p>Choose your favorite recipe and cook</p>
+        <p className={styles.cta}>
+          <Link href='/meals/share'>Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   )
 }
